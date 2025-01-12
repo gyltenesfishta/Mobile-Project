6b3a59cd5d916d34e3c64b1476652c66313ec5bb
gyltenesfishta <gyltene.sfishta@student.uni-pr.edu>
2025-01-13 09:19:44 +1400

    Small changes

--- app/src/main/java/com/example/project/SetPasswordActivity.java
+++ app/src/main/java/com/example/project/SetPasswordActivity.java
@@ -1,24 +1,57 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.regex.Pattern;
 
 public class SetPasswordActivity extends AppCompatActivity {
+    EditText newpass, confirmpass;
+    Button changepassword;
+    String email;
+    com.example.project.DB DB;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_set_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.img), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        newpass = findViewById(R.id.newpassword);
+        confirmpass = findViewById(R.id.confirmpassword);
+        changepassword = findViewById(R.id.changepassword);
+        email = getIntent().getStringExtra("Email");
+
+        DB = new com.example.project.DB(this);
+
+
+
+
+        changepassword.setOnClickListener(view->{
+            String reset1 = newpass.getText().toString().trim();
+            String reset2 = confirmpass.getText().toString().trim();
+            if(Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{6,20}$").matcher(reset1).matches())
+            {
+                if(reset1.equals(reset2)){
+                    DB.updateUserPassword(email, reset1);
+                    Toast.makeText(this, "Password updated successfully", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(this, com.example.project.SignupActivity.class));
+                }else{
+                    Toast.makeText(this, "Password don't match", Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                newpass.setError("Password must contain 1 lowercase, 1 uppercase, 1 digit, 1 special character, and be 6-20 characters long.");
+
+            }
+
         });
+
+
+
+
     }
 }
--- app/src/main/java/com/example/project/SignupActivity.java
+++ app/src/main/java/com/example/project/SignupActivity.java
@@ -1,24 +1,72 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SignupActivity extends AppCompatActivity {
+    Button btnCreateAccount;
+    EditText editSignupEmail;
+    EditText editSignupPassword;
+    TextView textView4;
+    Button btnLogin;
+    ImageView img;
+    com.example.project.DB DB;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_signup);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.img4), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+        editSignupEmail = findViewById(R.id.editSignupEmail);
+        editSignupPassword= findViewById(R.id.editSignupPassword);
+        textView4 = findViewById(R.id.textView4);
+        btnLogin = findViewById(R.id.btnLogin);
+        img = findViewById(R.id.img);
+
+        DB = new com.example.project.DB(this);
+
+        Animation pulse = AnimationUtils.loadAnimation(this, R.anim.pulse);
+        img.startAnimation(pulse);
+
+        btnLogin.setOnClickListener(view->{
+            String email2 = editSignupEmail.getText().toString().trim();
+            String password2 = editSignupPassword.getText().toString().trim();
+
+            if (email2.isEmpty() || password2.isEmpty()) {
+                Toast.makeText(SignupActivity.this, "Please enter both email and password", Toast.LENGTH_SHORT).show();
+                return; }
+            if (!DB.userExists(email2)) {
+                Toast.makeText(SignupActivity.this, "Account does not exist", Toast.LENGTH_SHORT).show();
+            } else if (!DB.validateUser(email2, password2)) {
+                Toast.makeText(SignupActivity.this, "Incorrect password", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent(this, Home_screenActivity.class);
+                intent.putExtra("USER_EMAIL", email2);
+                startActivity(intent);
+
+
+
+            }
+
         });
+
+        btnCreateAccount.setOnClickListener(view->{
+            startActivity(new Intent(this, SignupActivity.class));
+        });
+        textView4.setOnClickListener(view->{
+            startActivity(new Intent(this, Forgot_PasswordActivity.class));
+        });
+
     }
+
+
+
 }
